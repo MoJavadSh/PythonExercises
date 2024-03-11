eb25a689b037d60e2f473c72dd23a96d6848b794
MohammadJavad <mojavadssh@gmail.com>
2024-03-12 02:12:32 +0330

    Exercise 4 added

---
+++ Chapter2.py
@@ -0,0 +1,25 @@
+
+# Exercise 1
+x = 5
+y = 3
+print(x, y)
+x, y = y, x
+print(x, y)
+
+# Exercise 2
+x = 6
+y = 8
+print('area =', x*y)
+print('perimeter =', (x+y)*2)
+
+# Exercise 3
+Temperature = 1
+print((Temperature * 1.8) + 32)
+
+# Exercise 4
+Radius = 6
+print(Radius)
+print("perimeter =", 2*Radius*3.14)
+print("area =", 3.14*Radius**2)
+
+# Exercise 5
