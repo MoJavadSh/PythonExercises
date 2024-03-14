1fd878fe5890688715e0c751f5519573422c76dd
MohammadJavad <mojavadssh@gmail.com>
2024-03-14 23:39:41 +0330

    Exercise 3-1-2 added

--- ExerciseNum3.py
+++ ExerciseNum3.py
@@ -1,12 +1,21 @@
 # This file is or Exercises chapter3 and will name the exercises as the file in Ex_Program
 
 # Exercise 3-1-1
-radius = int(input("Enter the radius of the circle: "))
-area = radius * radius * 3.14
-perimeter = 2 * 3.14 * radius
+# radius = int(input("Enter the radius of the circle: "))
+# area = radius * radius * 3.14
+# perimeter = 2 * 3.14 * radius
+#
+# print("The area is", area)
+# print("The perimeter is", perimeter)
 
-print("The area is", area)
-print("The perimeter is", perimeter)
+# Exercise 3-1-2
+number = int(input("Enter the Number "))
+square = number ** 2
+cube = number ** 3
+print("The square is", square)
+print("The cube is", cube)
+
+
 
 
 
